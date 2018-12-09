7b9a0a6b67916b2848864376b24848023e5de9cb
Reader68 <kravvin@rocketsoftware.com>
2018-12-09 23:11:42 +0500

    It works!

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,21 @@
 package main;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
+import servlets.MirrorRequestServlet;
 
 public class Main {
     public static void main(String args[]) throws Exception{
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        MirrorRequestServlet mirrorRequestServlet = new MirrorRequestServlet();
 
         Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
         server.setHandler(context);
+        context.addServlet(new ServletHolder(mirrorRequestServlet), "/mirror");
         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
 
